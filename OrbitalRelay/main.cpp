a57a677ad985f220fc10f7d474b4599a4b9378ac
Inksaq <inksaq@protonmail.com>
2023-12-31 15:37:09 +1300

    Initial Commit

---
+++ OrbitalRelay/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OrbitalRelay
+//
+//  Created by Inksaq on 31/12/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
